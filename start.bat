a0ef3346a97b16366ca00d243f0cce75b18e7344
shikawa25 <felipelinscustodio@hotmail.com>
2022-12-15 15:56:28 -0300

    Revert "Initial commit"
    This reverts commit af72ada4580cdb704ad5ed18b8107b47123790ce.

--- start.bat
+++
@@ -1,6 +0,0 @@
-@echo off 
-echo Checking dependencies...
-pip freeze | Find "pyperclip==1.8.2" > NUL || pip install pyperclip==1.8.2
-pip freeze | Find "selenium==4.7.2" > NUL || pip install selenium==4.7.2
-python main.py
-pause
